efbceeb6ab320874101182569c3bb4cbbafe3dff
Edwin Flores <edwingioflo1396@gmail.com>
2021-12-14 17:34:48 -0500

    Update Calculadora.java
    Se realizan mejoras y refactor

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -3,30 +3,31 @@
  */
 public class Calculadora {
 
-    public int suma(int num1, int num2) {
+    //colocar el nombre correcto del metodo
+    public int sumar(int num1, int num2) {
         //metodo para crear una suma
-        int resul = 0;
-        resul = num1 + num2;
-        return resul;
+        //Se puede retornar directamente el resultado
+        return num1 + num2;
     }
 
-    public int resta(int num1, int num2) {
+    //colocar el nombre correcto del metodo
+    public int restar(int num1, int num2) {
         //metodo para crear la resta
-        int resul = 0;
-        resul = num1 - num2;
-        return resul;
+        //Se puede retornar directamente el resultado
+        return num1 - num2;
     }
 
-    public int multipli(int num1, int num2) {
+    //colocar el nombre correcto del metodo
+    public int multiplicar(int num1, int num2) {
         //metodo para crear la multiplicacion
-        int resul = 0;
-        resul = num1 * num2;
-        return resul;
+        //Se puede retornar directamente el resultado
+        return num1 * num2;
     }
-
-    public double divi(int num1, int num2) {
+    //colocar el nombre correcto del metodo
+    public double dividir(int num1, int num2) {
         //metodo para la division
         //double es para un valor con punto decimal
+        //Se deberia validar el caso en que num2 sea cero, se pueda usar excepciones
         double resul = 0;
         resul = num1 / num2;
         return resul;
